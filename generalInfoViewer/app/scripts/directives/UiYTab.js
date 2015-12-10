025e236913e70b1caca1813d60faf80c8150edc7
xudingjiemike <xudingjie_mike@163.com>
2015-12-10 19:26:07 +0800

    +

--- generalInfoViewer/app/scripts/directives/UiYTab.js
+++ generalInfoViewer/app/scripts/directives/UiYTab.js
@@ -1,3 +1,20 @@
 /**
  * Created by apple on 15/12/9.
  */
+angular.module('ui.yypt5.yhgl.GeneralInfoViewer')
+    .directive('uiYTab',function(){
+        return{
+            controller:'UiYTabController',
+            templateUrl:function(element,attr){
+                return attr.tempalteUrl || 'views/template/tabs/commonTab.html';
+            },
+            transclude:true,
+            replace:true,
+            scope:{
+                type:'@',
+                close:'&'
+            }
+
+        }
+    });
+
