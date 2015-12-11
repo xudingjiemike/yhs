19cf1438a91295074ea7047f99a6951e3d549a78
zhangj <zhangj@servyou.com.cn>
2015-12-11 10:30:16 +0800

    Signed-off-by: zhangj <zhangj@servyou.com.cn>

--- generalInfoViewer/app/scripts/controllers/InformationController.js
+++ generalInfoViewer/app/scripts/controllers/InformationController.js
@@ -5,18 +5,26 @@ angular
     .module('ui.yypt5.yhgl.GeneralInfoViewer.ContLeft')
     .controller('InformationController',function($scope){
         var isHover = false;
-        $scope.mouseLeaveEvent=function(){
-            $('.company-info').slideUp(300,function(){
-                $(".show-detail").removeClass("hovered");
-                isHover = false;
-            });
-        };
-
-        $scope.mouseOverEvent=function(){
+        $scope.showDetail=function(){
             if(!isHover){
                 isHover = true;
                 $('.show-detail').addClass("hovered");
                 $(".company-info").slideDown(300);
             }
         };
+        $scope.hideDetail=function(){
+            $('.company-info').slideUp(300,function(){
+                $(".show-detail").removeClass("hovered");
+                isHover = false;
+            });
+        };
+        $scope.showAgent=function(){
+            alert('222222222');
+            $(".layer").show();
+            $(".ztop").removeClass("ztop");
+            $(".delegate-search").addClass("ztop").show();
+        };
+        $scope.showManage=function(){
+
+        }
     });
