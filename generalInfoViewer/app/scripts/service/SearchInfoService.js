8e0871070a2bca1208d931b15f154dd633a60363
zhangj <zhangj@servyou.com.cn>
2015-12-16 17:04:05 +0800

    Signed-off-by: zhangj <zhangj@servyou.com.cn>

---
+++ generalInfoViewer/app/scripts/service/SearchInfoService.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Zhang on 2015/12/16.
+ */
+angular
+    .module('ui.yypt5.yhgl.GeneralInfoViewer.SearchInfo')
+    .service('SearchInfoService', ['_','SearchInfoApi',
+        function (_,SearchInfoApi) {
+
+        }]);
