1b22daff457625d44b0bd90f87af1527f583c3e3
xudingjiemike <xudingjie_mike@163.com>
2015-12-17 13:49:51 +0800

    +

--- generalInfoViewer/app/scripts/service/SoftwareAuthorityService.js
+++ generalInfoViewer/app/scripts/service/SoftwareAuthorityService.js
@@ -1,12 +1,10 @@
 /**
  * Created by apple on 15/12/16.
  */
-/**
- * Created by apple on 15/12/14.
- */
+
 angular
     .module('ui.yypt5.yhgl.GeneralInfoViewer')
-    .service('SoftwareAuthorityService',['_','SoftwareAuthorityApi',function(_,SoftwareAuthorityApi){;
+    .service('SoftwareAuthorityService',['_','SoftwareAuthorityApi',function(_,SoftwareAuthorityApi){
         var SoftwareAuthorityData = [];
         function LoadData(khid){
             SoftwareAuthorityApi.LoadSoftwareAuthorityInfo(khid).success(function(obj){
